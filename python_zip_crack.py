5710a2cb4d4fcc0eb1fce51d818c5306aa2fceea
ovens <ovenwlm123@gmail.com>
2014-06-29 11:50:48 +0800

    standard
    standard

--- python_zip_crack.py
+++ python_zip_crack.py
@@ -1,25 +1,27 @@
+#code=utf-8
+
 import zipfile
 import optparse
 from threading import Thread
 def extractFile(zFile,password):
     try:
         zFile.extractall(pwd=password)
-        print '[+] Found password ' + password +'\n'
+        print '[+] Found password  is ' + password +'\n'
     except:
         pass
 def main():
-    parser = optparse.OptionParser("usage%prog" + "-f <zipfile> -d <dictionary>")
-    parser.add_option('-f',dest = 'zname',type = 'string',help='specify zip file')
-    parser.add_option('-d',dest= 'dname',type='string',help='specify dictionary file ')
+    parser = optparse.OptionParser("usage%prog" + "-f <zipfile> -d <dictionary>")   
+    parser.add_option('-f',dest = 'zName',type = 'string',help='specify zip file')
+    parser.add_option('-d',dest= 'dName',type='string',help='specify dictionary file ')
     (options,args)=parser.parse_args()
-    if (options.zname == None) | (options.dname == None):
+    if (options.zName == None) | (options.dName == None):  
         print parser.usage
         exit(0)
     else:
-        zname = options.zname
-        dname = option.dname
-    zFile = zipfile.ZipFile(zname)
-    passFile = open(dname)
+        zName = options.zName
+        dName = option.dName
+    zFile = zipfile.ZipFile(zName)
+    passFile = open(dName)
     for line in passFile.readlines():
         password = line.strip('\n')
         t = Thread(target=extractFile,args=(zFile,password))
